31728c5e6a667fbcd9afd72d266aad7237eebb0d
kobevcxz <dirkkobe12@gmail.com>
2025-08-26 15:02:01 +0800

    Initial commit - course files

---
+++ 01-fundamentals-part-1/starter/script.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log(js);
+
+
+console.log("=== VARIABLES ===");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+console.log(age);
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
